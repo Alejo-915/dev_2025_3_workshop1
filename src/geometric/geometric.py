b43c8dc7cfd10349141319fc23e440049f1333fc
Alejo-915 <edwinlm915@gmail.com>
2025-09-04 00:41:20 -0500

    feat(geometric): área y perímetro de triángulo

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -17,11 +17,12 @@ class Geometria:
         return 2 * math.pi * radio
 
     # triangulo
-    def area_triangulo(self, base, altura):
+       def area_triangulo(self, base, altura):
         return (base * altura) / 2
 
     def perimetro_triangulo(self, lado1, lado2, lado3):
         return lado1 + lado2 + lado3
+
 
     def es_triangulo_valido(self, lado1, lado2, lado3):
         return (lado1 + lado2 > lado3 and
